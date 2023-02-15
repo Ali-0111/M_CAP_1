79a93b27113efb2e967b0b3b8148e38636804b0b
Ali01 <alirahanet@gmail.com>
2023-02-15 23:20:51 +0430

    initial container added for popup menu

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Hi, how are you....");
+const body = document.querySelector('body');
+const humberger = document.querySelector('.humberger');
+
+
+humberger.onclick = ()=> {
+    createSection();
+};
+
+//      ------------  functions   -----------    
+
+function createSection() {
+    const section = document.createElement('section');
+    section.classList = 'popup';
+    const divForPop = document.createElement('div');
+    divForPop.classList = 'popup-content';
+    divForPop.innerHTML = `
+    <button
+    class="humberger2 navbar-toggler postion-fixed mt-3     toggler-example " type="button">
+        <span class="span-1"></span>
+        <span class="hide"></span>
+        <span class="span-2"></span>
+    </button>
+    `;
+    section.append(divForPop);
+    body.appendChild(section);
+    remove(section);
+};
+
+function remove (o) {
+    const humberger2 = document.querySelector('.humberger2');
+    humberger2.onclick = () => {
+        body.removeChild(o);
+    };
+}

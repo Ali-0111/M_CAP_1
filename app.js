79c05ca5b8170dc1c9330d21f1c42b6d1332cb93
Ali01 <alirahanet@gmail.com>
2023-02-16 15:10:16 +0430

    dynamically speakers section created

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const body = document.querySelector('body');
 const humberger = document.querySelector('.humberger');
+const people = document.querySelector('.people');
+const data = [
+    {
+        name:'Oliver Rolland',
+        position:'Managing Director chez Toulouse White Biotechnology (TWB)',
+        idea: 'He has recieved the Presidential Green Chemistry Award 2014 for “Farnesane: a Breakthrough Renewable Hydrocarbon for Use as Diesel and Jet Fuel” ',
+        picture: './img/profiles/person2.jpeg',
+        pattern: './img/chess.jpg'
+    },
+    {
+        name:'Irina Borodina',
+        position:'Professor, Novo Nordisk Foundation Center for Biosustainability',
+        idea: 'Research interests: Metabolic Engineering, Synthetic Biology, Industrial Biotechnology',
+        picture: './img/profiles/person3.jpeg',
+        pattern: './img/chess.jpg'
+    },
+    {
+        name:'Greg De Temmerman',
+        position:'Managing Director at Zenon Research. Associate Researcher at Mines Paris Tech/IHEIE',
+        idea: 'Leading scientist with a demonstrated history of working in an international and challenging project environment. Author of more than 230 scientific articles',
+        picture: './img/profiles/person4.jpeg',
+        pattern: './img/chess.jpg'
+    },
+    {
+        name:'Ann Mettler',
+        position:'Vice President, Europe at Breakthrough Energy',
+        idea: 'Ann Mettler is the head of the European Political Strategy Centre (EPSC), the in-house think tank of the European Commission.',
+        picture: './img/profiles/person5.jpeg',
+        pattern: './img/chess.jpg'
+    },
+    {
+        name:'Angeleki Rigos',
+        position:'President, Europe at Breakthrough Energy.',
+        idea: 'Angeleki Rigos is the President of the European Political Strategy Centre (EPSC), the in-house think tank of the European Commission.',
+        picture: './img/profiles/person6.jpeg',
+        pattern: './img/chess.jpg'
+    }
+]
 
 
-// ---------------------- events -----------------------
-humberger.onclick = ()=> {
-    createSection();
-};
+
 
 //      ------------  functions   -----------    
 
@@ -51,12 +86,118 @@ function remove(o) {
     };
 }
 
-// function arrayEvent(arr, section) {
-//     // Array.from(arr).forEach((link)=> {
-//     //     link.onclick = () => {
-//     //         remove(section);
-//     //     };
-//     // });
+function createSpeaker() {
+    for (let i= 0; i < 2; i++)
+    {
+    const li = document.createElement('li');
+    li.classList = 'd-flex my-4';
+    li.innerHTML = `
+    <div class="profile position-relative d-flex justfify-content-center align-items-center">
+                        <img class="img1 position-absolute top-0 left-0" src="${data[i].pattern}" alt="chessPattern">
+                        <img class="img2 mt-2 ms-3 z-3" src="${data[i].picture}" alt="person2">
+                    </div>
+                    <div class="idea ps-5 align-self-center">
+                            <h3 class="display-6 fw-semibold">${data[i].name}</h3>
+                        <p class="text-danger fst-italic fs-5 mb-0">${data[i].position}</p>
+                        <div class="space my-3"></div>
+                        <p class="research fs-4">
+                            ${data[i].idea}
+                        </p>
+                    </div>
+    `;
+    people.append(li);
+    }
+    createChevronBtn();
+    collpase();
+};
+// ------------- function for creatine Chevtron btn --------------
+function createChevronBtn() {
+    const btn = document.createElement('div');
+    btn.classList = 'w-100';
+    btn.innerHTML = `
+        <button class="d-flex border bg-white w-100 py-3 my-3 fs-2 justify-content-center"
+                    type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
+                    >
+                        MORE
+                        <svg class="p mx-2" width="16" height="28"  fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                </button>
+    `;
+    people.append(btn);   
+}
 
-//     console.log("insed");
-// }
+// ---------------- function for creatine chevetron collapse menu ---------
+
+function collpase() {
+    const div = document.createElement('div');
+    div.classList = 'collapse';
+    div.id = 'collapseExample';
+    for (let i = 2; i< data.length ;i++)
+    div.innerHTML = `
+    
+    <li class="d-flex my-4">
+    <div class="profile position-relative d-flex justfify-content-center align-items-center">
+        <img class="img1 position-absolute top-0 left-0" src="./img/chess.jpg" alt="chessPattern">
+        <img class="img2 mt-2 ms-3 z-3" src="./img/profiles/person4.jpeg" alt="person4">
+    </div>
+    <div class="idea ps-5 align-self-center">
+            <h3 class="display-6 fw-semibold">
+                Greg De Temmerman
+            </h3>
+        <p class="text-danger fst-italic fs-5 mb-0">
+            Managing Director at Zenon Research. Associate Researcher at Mines Paris Tech/IHEIE
+        </p>
+        <div class="space my-3"></div>
+        <p class="research fs-4">     
+            Leading scientist with a demonstrated history of working in an international and challenging project environment. Author of more than 230 scientific articles
+        </p>
+    </div>
+    </li>
+    <li class="d-flex my-4">
+    <div class="profile position-relative d-flex justfify-content-center align-items-center">
+        <img class="img1 position-absolute top-0 left-0" src="./img/chess.jpg" alt="chessPattern">
+        <img class="img2 mt-2 ms-3 z-3" src="./img/profiles/person5.jpeg" alt="person5">
+    </div>
+    <div class="idea ps-5 align-self-center">
+            <h3 class="display-6 fw-semibold">
+                Ann Mettler
+            </h3>
+        <p class="text-danger fst-italic fs-5 mb-0">
+            Vice President, Europe at Breakthrough Energy
+        </p>
+        <div class="space my-3"></div>
+        <p class="research fs-4">     
+            Ann Mettler is the head of the European Political Strategy Centre (EPSC), the in-house think tank of the European Commission.
+        </p>
+    </div>
+    </li>
+    <li class="d-flex my-4">
+    <div class="profile position-relative d-flex justfify-content-center align-items-center">
+        <img class="img1 position-absolute top-0 left-0" src="./img/chess.jpg" alt="chessPattern">
+        <img class="img2 mt-2 ms-3 z-3" src="./img/profiles/person6.jpeg" alt="person6">
+    </div>
+    <div class="idea ps-5 align-self-center">
+            <h3 class="display-6 fw-semibold">
+                Angeleki Rigos
+            </h3>
+        <p class="text-danger fst-italic fs-5 mb-0">
+            Vice President, Europe at Breakthrough Energy.
+        </p>
+        <div class="space my-3"></div>
+        <p class="research fs-4">     
+            Ann Mettler is the head of the European Political Strategy Centre (EPSC), the in-house think tank of the European Commission. 
+        </p>
+    </div>
+    </li>    
+    `;
+    people.append(div);
+}
+// ---------------------- events -----------------------
+humberger.onclick = ()=> {
+    createSection();
+};
+
+window.onload = ()=> {
+    createSpeaker();
+};
